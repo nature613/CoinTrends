03c66db961ef7658936b1376abf158db1dfc1f39
nature613 <chikonakamura613@gmail.com>
2018-09-03 21:10:29 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+// Summary of commands
+//  gulp watch:
+//    Initiate browser-sync,
+//    Watch html/css/js files,
+//    Compile watched .scss files into .css files on save,
+//    Refresh browser-sync on saving watched files.
+//
+//  gulp build:
+//    Delete contents of public/ ready for rebuild,
+//    Compile all .scss files into .css files and copy .css into public/assets/,
+//    Concatenate and minify .css and .js files and publish index.html into public/,
+//    Copy images from dev/assets/images/ into public/assets/images/.
+//    
+
+// requires:
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
+//
+
+// // // 'gulp watch' tasks including preprocessing css // // // 
+
+// browserSync - initiates browserSync
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: 'dev'
+    },
+    port: 8080
+  });
+});
+
+// sass - compiles sass files into css, and re-injects css into browserSync window
+gulp.task('sass', function(){
+  return gulp.src('dev/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dev/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// watch - watches html/css/js files in dev/**/*, and reloads browserSync when any watched file is saved
+//   when a watched scss file is saved, all scss files are compiled into css files
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('dev/scss/**/*.scss', ['sass']);
+  gulp.watch('dev/*.html', browserSync.reload);
+  gulp.watch('dev/js/**/*.js', browserSync.reload);
+});
+
+// // //                                              // // //
+
+
+
+// // // 'gulp build' tasks including concatenating & minifying .js and .css files // // // 
+// // also depends on 'gulp sass', from gulp watch section above
+
+// clear:public - deletes everything in public/ directory, to clean up unused files before public/ can be rebuilt
+gulp.task('clear:public', function() {
+  return del.sync(['public/**', '!public']); //must ignore parent directory if you don't want it to be deleted too
+});
+
+// useref - copies html files from dev/ to public/,
+//   also concatenates and minifies .css and .js files, and places resulting files in public/assets/
+gulp.task('useref', function(){
+  return gulp.src('dev/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('public'));
+});
+
+// fonts - copies fonts from dev/fonts/ to public/fonts/
+//  temporarily removed since I'm no longer using downloaded fonts
+/*gulp.task('fonts', function() {
+  return gulp.src('dev/assets/fonts/*')
+  .pipe(gulp.dest('public/assets/fonts'));
+});*/
+
+// images - copies images from dev/images to public/images/
+//   separate from gulp fonts, in case I want to replace this with imagemin in the future, to compress images.
+//   currently not planning on having any large images in this project, though.
+gulp.task('images', function(){
+  return gulp.src('dev/assets/images/*')
+  .pipe(gulp.dest('public/assets/images'));
+});
+
+// build - runs a sequence of gulp tasks to rebuild public/:
+//   Compile sass, copy html to public/ & concatenate/minify .css/.js, copy images and fonts to public/assets
+gulp.task('build', function (callback) {
+  runSequence('clear:public', 
+    /*['sass', 'useref', 'fonts', 'images'],*/ // for if I end up using fonts again
+    ['sass', 'useref', 'images'],
+    callback
+  );
+});
+
+// // //                                                                       // // //
