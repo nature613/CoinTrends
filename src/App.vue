e8fe4dad6f2e8cfb0e8c5366dbb92365f612fc3b
nature613 <chikonakamura613@gmail.com>
2019-12-07 23:40:57 +0000

    generate new version of project from Vue CLI

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    zxcg
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
