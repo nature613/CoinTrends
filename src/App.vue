8abd46f38b83040aadf171ff0a9391b8e9467e36
nature613 <chikonakamura613@gmail.com>
2020-06-15 17:24:11 +0100

    Set-up of the site structure: add info/assets to <head> in index.html, add some generic css to global.scss, variables.scss and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    zxcg
   </div>
 </template>
 
@@ -12,8 +11,24 @@ export default {
 </script>
 
 <style lang="scss">
+@import "assets/css/minireset.css";
+@import "assets/css/variables.scss";
+@import "assets/css/global.scss";
 #app {
+  font-family: 'rawline', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-size: 16px;
+  box-sizing: border-box;
+  color: $textColor;
+  @media (max-width: $breakpoint-med) {
+    font-size: 14px;
+  }
+  @media (max-width: $breakpoint-small) {
+    font-size: 12px;
+  }
+}
+*, *:before, *:after {
+  box-sizing: inherit;
 }
 </style>
